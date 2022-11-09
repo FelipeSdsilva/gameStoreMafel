2c970a1b6ce44393fc723e49f8257d07334eb4f6
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-09 19:35:13 -0300

    Seedind database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,20 @@
+INSERT INTO tb_game (title,description,price,image,link_Video,qtd_Stok,metacritic_Note,date_Lanc) VALUES ('God of War Ragnarok','Reserve já para receber os seguintes itens do jogo*: - Armadura da Nevintensa do Kratos. - Túnica da Nevintensa do Atreus (cosmético). *Desbloqueie itens do jogo ao progredir na história. Do Santa Monica Studio, esta é a sequência da aclamada versão de 2018 de God of War. Junte-se a Kratos e Atreus em uma jornada mítica à procura de respostas na iminência do Ragnarök. Juntos, pai e filho arriscarão tudo viajando por cada um dos Nove Reinos. Explorando paisagens mitológicas de tirar o fôlego, eles enfrentarão deuses nórdicos, feras selvagens e outros inimigos aterradores enquanto se preparam para o maior confronto de suas vidas. Kratos será desafiado como nunca e usará as suas poderosas armas de guerra, incluindo o Machado Leviatã e as Lâminas do Caos, para proteger sua família. Com a variedade de novas habilidades para ele e Atreus, o combate nesta odisseia épica e implacável é fluido, flexível e impactante.Enquanto isso, as forças de Asgard se preparam... - Viva a jornada pelos reinos nórdicos com a imersão da resposta tátil e dos gatilhos adaptáveis. - Aprecie a beleza dos mundos criados por uma direção de arte impecável com detalhes minuciosos.- Aproveite o áudio em 3D multidirecional; ouça os inimigos vindo de qualquer direção.¹ ¹Áudio em 3D com fones de ouvido estéreo (analógico ou USB).',349.90,'https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png','https://www.youtube.com/watch?v=gqciogn3sUc&ab_channel=PlayStationBrasil',100,94.0,TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z');
+
+INSERT INTO tb_platform (name) VALUES ('PS5');
+INSERT INTO tb_platform (name) VALUES ('PS4');
+INSERT INTO tb_platform (name) VALUES ('Nintendo Switch');
+INSERT INTO tb_platform (name) VALUES ('PC');
+INSERT INTO tb_platform (name) VALUES ('Mobile');
+INSERT INTO tb_platform (name) VALUES ('Nintendo 3DS');
+INSERT INTO tb_platform (name) VALUES ('Xbox S|S');
+INSERT INTO tb_platform (name) VALUES ('Xbox S|X');
+INSERT INTO tb_platform (name) VALUES ('Xbox One');
+
+INSERT INTO tb_genre(name) VALUES ('Action');
+INSERT INTO tb_genre(name) VALUES ('RPG');
+INSERT INTO tb_genre(name) VALUES ('Strategy');
+INSERT INTO tb_genre(name) VALUES ('Aventure');
+INSERT INTO tb_genre(name) VALUES ('Simulation');
+INSERT INTO tb_genre(name) VALUES ('Sports and Cars');
+INSERT INTO tb_genre(name) VALUES ('Figth');
+
